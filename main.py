eee2e9902788ca6d519ca6d959695cae056b3d7e
JeanCamargoReal <jeancamargoreal@gmail.com>
2018-10-03 15:00:29 -0300

    Widgets com propriedades reativas

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from entidades import cliente
 from repositorios import cliente_repositorio
 
-cliente = cliente.Cliente("Samantha", 25)
+# cliente = cliente.Cliente("Samantha", 25)
 
-cliente_repositorio.ClienteRepositorio.listar_clientes()
+# cliente_repositorio.ClienteRepositorio.listar_clientes()
 
 
