59be742b81a4e08822a941ed0c2a6695de49fa4d
JeanCamargoReal <jeancamargoreal@gmail.com>
2018-10-03 12:40:14 -0300

    Criando o primeiro Widget

---
+++ gui/teste.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+# from kivy.uix.button import Button
+# from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+
+class Principal(Widget):
+    pass
+
+
+class Secundario(Widget):
+    pass
+
+
+class Teste(App):
+    def build(self):
+        return Secundario()
+
+
+Teste().run()
