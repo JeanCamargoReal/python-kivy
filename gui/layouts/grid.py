74d4f3fc709be45c86a20c34da2ea5b18770fde1
JeanCamargoReal <jeancamargoreal@gmail.com>
2018-10-04 10:50:59 -0300

    Trabalhando com GridLayout

---
+++ gui/layouts/grid.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class TelaApp(GridLayout):
+    pass
+
+
+class Grid(App):
+    def build(self):
+        return TelaApp()
+
+
+Grid().run()
